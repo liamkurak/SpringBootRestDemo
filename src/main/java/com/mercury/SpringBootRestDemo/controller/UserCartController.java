d964f4458ae7b9de22b3ba0e381443b52f862ca8
Liam K <liamzlyii@gmail.com>
2023-03-28 19:57:21 -0500

    add new Compnent

---
+++ src/main/java/com/mercury/SpringBootRestDemo/controller/UserCartController.java
@@ -0,0 +1,39 @@
+package com.mercury.SpringBootRestDemo.controller;
+
+import com.mercury.SpringBootRestDemo.bean.AddToCart;
+import com.mercury.SpringBootRestDemo.service.AddToCartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/addtocart")
+public class AddToCartController {
+
+    @Autowired
+    private AddToCartService addToCartService;
+
+    @GetMapping
+    public List<AddToCart> getAll(){
+        System.out.println("AddToCartController -> getAll(): "+addToCartService.getAll());
+//        addToCartService.getAll();
+        return addToCartService.getAll();
+    }
+
+    @PutMapping
+    public void PutToCart(@RequestBody AddToCart addToCart) {
+        // PUT - http://localhost:8080/addtocart
+        addToCartService.save(addToCart);
+    }
+
+    @PostMapping
+    public void save(@RequestBody AddToCart addToCart) {
+        // POST - http://localhost:8080/comments
+        addToCartService.save(addToCart);
+    }
+
+
+
+
+}
